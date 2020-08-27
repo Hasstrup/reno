05ff7543b112bb6cca2521e27c89852f89f92079
Hasstrup Ezekiel <hasstrup.ezekiel@gmail.com>
2020-08-27 01:50:59 +0100

    [chore] renders child nodes correctly

--- src/Pages/CompanyListPage/Components/CompanyListItem/CompanyListItem.styles.ts
+++ src/Pages/CompanyListPage/Components/CompanyListItem/CompanyListItem.styles.ts
@@ -1,36 +1,69 @@
-import {createUseStyles} from 'react-jss';
+import { createUseStyles } from "react-jss";
 
 export const CompanyListItemStyles = createUseStyles({
-    CardContainer: {
-        padding: 0,
-        '& .ant-card-body': {
-            padding: '0 !important'
-        },
-        '& .ant-card-head': {
-            height: '100px'
-        }
+  CardContainer: {
+    padding: 0,
+    "& .ant-card-body": {
+      padding: "0 !important",
     },
-    HeaderImage: {
-        height: '50px',
-        borderRadius: '50%',
-        width: '50px'
+    "& .ant-card-head": {
+      height: "100px",
     },
-    Divider: {
-        margin: 0
+  },
+  HeaderImage: {
+    height: "50px",
+    borderRadius: "50%",
+    width: "50px",
+  },
+  MainText: {
+    textAlign: "left !important",
+    paddingTop: "2px",
+  },
+  Divider: {
+    margin: 0,
+  },
+  Text: {
+    textAlign: "center",
+    fontSize: "12px",
+  },
+  ChildRow: {
+      paddingLeft: '40px'
+  },
+  ImageContainer: {
+    display: "flex",
+    flexFlow: "column nowrap",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  IconContainer: {
+    height: "20px",
+    position: "absolute",
+    right: "5px",
+  },
+  RowContainer: {
+    height: "150px",
+  },
+  CompanyEntryContainer: {
+    paddingBottom: "2%",
+  },
+  Row: {
+    height: "40px",
+    paddingTop: "7px",
+  },
+  ContentContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    "& .ant-progress-inner": {
+      width: "30px !important",
+      height: "30px !important",
+      fontSize: "12px !important",
     },
-    Text: {
-        textAlign: 'center',
-        fontSize: '12px'
-    },
-    ImageContainer: {
-        display: 'flex',
-        flexFlow: 'column nowrap',
-        justifyContent: 'flex-start',
-        alignItems: 'center'
-    },
-    IconContainer: {
-        height: '20px',
-        position: 'absolute',
-        right: '5px'
-    }
-})
+  },
+  CloseIcon: {
+    color: "red",
+  },
+  Caret: {
+    fontSize: "12px",
+  },
+});
--- src/Pages/CompanyListPage/Components/CompanyListItem/CompanyListItem.tsx
+++ src/Pages/CompanyListPage/Components/CompanyListItem/CompanyListItem.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
-import { Card, Typography, Divider, Row, Col, Image } from "antd";
+import { Card, Typography, Divider, Row, Col, Image, Progress } from "antd";
+import clsx from "classnames"
 import {
   RightOutlined,
   CloseCircleFilled,
@@ -8,26 +9,24 @@ import {
 import { CompanyListItemStyles } from "./CompanyListItem.styles";
 import { CompanyListItemProps } from "./CompanyListItem.types";
 
-// style ant card body to reduce padding
-// padding 24px on antlist item
 export const CompanyListItem: FC<CompanyListItemProps> = (props) => {
   const classes = CompanyListItemStyles();
 
   const HeaderTitle = (props) => {
     return (
-      <Row style={{ height: "200px" }}>
+      <Row className={classes.RowContainer}>
         <Col span={4}></Col>
         <Col span={16} className={classes.ImageContainer}>
-            <Image
-              className={classes.HeaderImage}
-              src={
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png"
-              }
-            />
+          <Image
+            className={classes.HeaderImage}
+            src={
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png"
+            }
+          />
           <Typography className={classes.Text}>Google</Typography>
         </Col>
         <Col span={4} className={classes.IconContainer}>
-            <CloseOutlined />
+          <CloseOutlined />
         </Col>
       </Row>
     );
@@ -35,31 +34,52 @@ export const CompanyListItem: FC<CompanyListItemProps> = (props) => {
 
   return (
     <>
-      <Card
-        title={
-          <span>
-            <HeaderTitle />
-          </span>
-        }
-        className={classes.CardContainer}
-      >
-        <Row>
-          <Col span={4}>
-            <RightOutlined />
+      <Card title={<HeaderTitle />} className={classes.CardContainer}>
+        {/* { This should start the items} */}
+        <Row className={clsx(classes.Row)}>
+          <Col span={2}>
+            <RightOutlined className={classes.Caret} size={8}/>
           </Col>
-          <Col span={16}>
-            <Typography.Paragraph className={classes.Text}>
+          <Col span={20}>
+            <Typography.Paragraph className={clsx(classes.Text, classes.MainText)}>
               {"Something"}
             </Typography.Paragraph>
           </Col>
-          <Col span={4}>
-            <CloseCircleFilled />
+          <Col span={2}>
+            <CloseCircleFilled className={classes.CloseIcon} />
           </Col>
         </Row>
-        <Divider className={classes.Divider}/>
-        <Typography.Paragraph style={{ fontSize: "12px" }}>
-          {"Something"}
-        </Typography.Paragraph>
+        <Divider className={classes.Divider} />
+        {/* Show children here */}
+        <Row className={clsx(classes.Row, classes.ChildRow)}>
+          <Col span={20}>
+            <Typography.Paragraph className={clsx(classes.Text, classes.MainText)}>
+              {"Something"}
+            </Typography.Paragraph>
+          </Col>
+        </Row>
+        <Divider className={classes.Divider} />
+        <Row className={clsx(classes.Row, classes.ChildRow)}>
+          <Col span={20}>
+            <Typography.Paragraph className={clsx(classes.Text, classes.MainText)}>
+              {"Something"}
+            </Typography.Paragraph>
+          </Col>
+        </Row>
+
+        {/* End of item */}
+        <Divider className={classes.Divider} />
+        <Row className={clsx(classes.Row, classes.CompanyEntryContainer)}>
+          {/* <Col span={2}>
+            <RightOutlined />
+          </Col> */}
+          <Col span={24} className={classes.ContentContainer}>
+            <Progress type="circle" percent={75} format={(percent: any) => `${percent / 10}`}/>
+          </Col>
+          {/* <Col span={2}>
+            <CloseCircleFilled />
+          </Col> */}
+        </Row>
       </Card>
     </>
   );
